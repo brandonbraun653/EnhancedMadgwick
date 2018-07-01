f197e7d16188f16852745b44120a036cada8b5d8
Brandon Braun <brandonbraun653@gmail.com>
2018-06-30 21:21:44 -0500

    First commit, added boilerplate

---
+++ enhancedMadgwick.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef ENHANCED_MADGWICK_HPP
+#define ENHANCED_MADGWICK_HPP
+
+
+class EnhancedMadgwick
+{
+public:
+
+	EnhancedMadgwick() = default;
+	~EnhancedMadgwick() = default;
+
+private:
+
+};
+
+#endif /* !ENHANCED_MADGWICK_HPP */
